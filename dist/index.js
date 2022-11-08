14fbca0eebef44bbd1d52aa496653bddcd155673
MrEidu <a320670@uach.mx>
2022-11-08 13:17:04 -0600

    adding express and testing specs

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,21 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const myFunc = (num) => {
-    return num * num;
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-exports.default = myFunc;
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const port = 5000;
+//send enpoint
+app.get('/api', (req, res) => {
+    res.send("Hello World");
+});
+//check for port
+app.listen(port, () => console.log(`Listening on port  ${port}!`));
+exports.default = app;
+/*
+const myFunc = (num: number): number => {
+  return num * num;
+};
+
+export default myFunc;*/ 
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
+import express from "express";
+
+const app = express();
+const port = 5000;
+
+//send enpoint
+app.get('/api', (req, res) => {
+res.send("Hello World");
+});
+
+//check for port
+app.listen(port, () => console.log(`Listening on port  ${port}!`));
+
+export default app;
+
+/*
 const myFunc = (num: number): number => {
   return num * num;
 };
 
-export default myFunc;
+export default myFunc;*/
